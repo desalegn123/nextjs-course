9cc5372e2b261b72aae37de88beba2f5e9a6d8c9
Desalegn <sisay963646@gmail.com>
2024-11-29 10:46:40 +0200

    commit through practice

---
+++ src/app/products/[productid]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductDetails({params,}:{params:{productid:string}}){
+    return <h1>Details of all product {params.productid}</h1>
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductList(){
+    return <>
+    <h1>list of products</h1>
+    <h4> product 1</h4>
+    <h4> product 2</h4>
+    <h4> product 3</h4>
+    </>
+}
